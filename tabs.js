9fb8cecc6d238fa558d15f0921460f889a4c728b
Jen Bird <jenniredfield@hotmail.com>
2020-07-09 19:41:54 +0100

    Formatted with Prettier, removed console logs

--- tabs.js
+++ tabs.js
@@ -1,157 +1,151 @@
-console.log('log from tabs.js');
+console.log("log from tabs.js");
 
-document.onload  = (() => {
+document.onload = (() => {
+  function createTabs() {
+    const TABS_FROM_CMS = [
+      {
+        tabId: "tab-1",
+        tabTitle: "Tab 1",
+        contentTitle: "Title 1",
+        content: "Content 1",
+      },
+      {
+        tabId: "tab-2",
+        tabTitle: "Tab 2",
+        contentTitle: "Title 2",
+        content: "Content 2",
+      },
+      {
+        tabId: "tab-3",
+        tabTitle: "Tab 3",
+        contentTitle: "Title 3",
+        content: "Content 3",
+      },
+    ];
 
-    function createTabs() {
-        const TABS_FROM_CMS = [
-            {
-                tabId: 'tab-1', 
-                tabTitle: 'Tab 1',
-                contentTitle: 'Title 1',
-                content: 'Content 1'
-            },
-            {
-                tabId: 'tab-2', 
-                tabTitle: 'Tab 2',
-                contentTitle: 'Title 2',
-                content: 'Content 2'
-            },
-            {
-                tabId: 'tab-3', 
-                tabTitle: 'Tab 3',
-                contentTitle: 'Title 3',
-                content: 'Content 3'
-            },
-        ];
+    // create tabs parent
 
-        const wrapper = document.querySelector('.cms__container');
-        const tabsParent = document.createElement('div');
-        tabsParent.setAttribute('aria-label', 'Challenge Tabs');
-        tabsParent.setAttribute('role', 'tablist');
-        tabsParent.classList.add('cms__tabs-list-container')
-        wrapper.append(tabsParent);
+    const wrapper = document.querySelector(".cms__container");
+    const tabsParent = document.createElement("div");
+    tabsParent.setAttribute("aria-label", "Challenge Tabs");
+    tabsParent.setAttribute("role", "tablist");
+    tabsParent.classList.add("cms__tabs-list-container");
+    wrapper.append(tabsParent);
 
-        // const tabsParent = document.querySelector('[role="tablist"]');
+    TABS_FROM_CMS.forEach((tabData, i) => {
+      // create buttons
+      const button = document.createElement("button");
+      button.classList.add("cms__tabs-list-button");
+      button.setAttribute("id", tabData.tabId);
+      button.setAttribute("role", "tab");
+      button.setAttribute("tabindex", "-1");
+      button.setAttribute(
+        "aria-controls",
+        tabData.tabId.replace("tab", "content")
+      );
+      button.setAttribute("aria-selected", false);
+      button.textContent = tabData.tabTitle;
 
-        TABS_FROM_CMS.forEach((tabData, i) => {
-            // create buttons
+      if (i === 0) {
+        button.setAttribute("tabindex", "0");
+        button.setAttribute("aria-selected", true);
+      }
 
-            const button = document.createElement('button');
-            button.classList.add('cms__tabs-list-button');
-            button.setAttribute('id', tabData.tabId);
-            button.setAttribute('role', 'tab');
-            button.setAttribute('tabindex', '-1');
-            button.setAttribute('aria-controls', tabData.tabId.replace('tab', 'content'));
-            button.setAttribute('aria-selected', false);
-            button.textContent = tabData.tabTitle;
+      tabsParent.append(button);
+    });
 
-            if (i === 0) {
-                button.setAttribute('tabindex', '0');
-                button.setAttribute('aria-selected', true);
-            }
+    // create content
+    const contentContainer = document.createElement("div");
+    contentContainer.classList.add("cms__tabs-content-container");
+    wrapper.append(contentContainer);
 
-            tabsParent.append(button);
-        });
+    TABS_FROM_CMS.forEach((tabData, i) => {
+      const div = document.createElement("div");
+      div.classList.add("cms__tabs-content-item");
+      div.setAttribute("id", tabData.tabId.replace("tab", "content"));
+      div.setAttribute("role", "tabpanel");
+      div.setAttribute("tabindex", 0);
+      div.setAttribute("aria-labelledby", tabData.tabId);
 
-        // create content
- 
-        const contentContainer = document.createElement('div');
-        contentContainer.classList.add('cms__tabs-content-container');
-        wrapper.append(contentContainer);
+      const h2 = document.createElement("h2");
+      h2.innerText = tabData.contentTitle;
+      div.append(h2);
 
-     
-        TABS_FROM_CMS.forEach((tabData, i )=> {
-            const div = document.createElement('div');
-            div.classList.add('cms__tabs-content-item');
-            div.setAttribute('id', tabData.tabId.replace('tab', 'content'));
-            div.setAttribute('role', 'tabpanel');
-            div.setAttribute('tabindex', 0);
-            div.setAttribute('aria-labelledby', tabData.tabId);
+      const p = document.createElement("p");
+      p.innerText = tabData.content;
+      div.append(p);
 
-            const h2 = document.createElement('h2');
-            h2.innerText = tabData.contentTitle;
-            div.append(h2);
+      if (i !== 0) {
+        div.setAttribute("hidden", true);
+      }
 
-            const p = document.createElement('p');
-            p.innerText = tabData.content;
-            div.append(p);
+      contentContainer.append(div);
+    });
 
-            if(i !== 0) {
-                div.setAttribute('hidden', true)
-            }
+    // add listeners
+    addListeners();
+  }
 
-            contentContainer.append(div);
-        });
+  createTabs();
 
-        addListeners()
-    }   
+  function addListeners() {
+    const tabsParent = document.querySelector('[role="tablist"]');
+    const tabs = document.querySelectorAll('[role="tab"]');
+    const contentEls = document.querySelectorAll('[role="tabpanel"]');
 
+    function updateTab(e) {
+      // update tab selected
+      const targetId = e.target.id;
 
-    createTabs()
-
-
-    function addListeners() {
-        const tabsParent = document.querySelector('[role="tablist"]');
-        const tabs = document.querySelectorAll('[role="tab"]');
-        const contentEls = document.querySelectorAll('[role="tabpanel"]');
-    
-        function updateTab(e) {
-            // update tab selected
-            const targetId = e.target.id;
-    
-            tabs.forEach(tab => {
-                const tabId = tab.getAttribute('id');
-                if(tabId === targetId) {
-                    tab.setAttribute('aria-selected', true);
-                    return;
-                }
-    
-                tab.setAttribute('aria-selected', false);
-            });
-    
-            // update content selected
-            contentEls.forEach(contentEl => {
-                const ariaControlledBy = contentEl.getAttribute('aria-labelledby');
-                console.log("updateTab -> ariaControlledBy", ariaControlledBy)
-                
-                if (ariaControlledBy === targetId) {
-                    contentEl.removeAttribute('hidden');
-                    return;
-                }
-                contentEl.setAttribute('hidden', true);
-            })
-    
+      tabs.forEach((tab) => {
+        const tabId = tab.getAttribute("id");
+        if (tabId === targetId) {
+          tab.setAttribute("aria-selected", true);
+          return;
         }
-    
-        tabs.forEach(tab => {
-            tab.addEventListener('click', updateTab);
-        })
-    
-    
-        let tabFocus = 0;
-    
-        tabsParent.addEventListener("keydown", e => {
-        
-          if (e.keyCode === 39 || e.keyCode === 37) {
-            tabs[tabFocus].setAttribute("tabindex", -1);
-            if (e.keyCode === 39) {
-              tabFocus++;
-        
-              if (tabFocus >= tabs.length) {
-                tabFocus = 0;
-              }
-       
-            } else if (e.keyCode === 37) {
-              tabFocus--;
-     
-              if (tabFocus < 0) {
-                tabFocus = tabs.length - 1;
-              }
-            }
-      
-            tabs[tabFocus].setAttribute("tabindex", 0);
-            tabs[tabFocus].focus();
-          }
-        });
+
+        tab.setAttribute("aria-selected", false);
+      });
+
+      // update content selected
+      contentEls.forEach((contentEl) => {
+        const ariaControlledBy = contentEl.getAttribute("aria-labelledby");
+        console.log("updateTab -> ariaControlledBy", ariaControlledBy);
+
+        if (ariaControlledBy === targetId) {
+          contentEl.removeAttribute("hidden");
+          return;
+        }
+        contentEl.setAttribute("hidden", true);
+      });
     }
-})()
+
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", updateTab);
+    });
+
+    let tabFocus = 0;
+
+    tabsParent.addEventListener("keydown", (e) => {
+      if (e.keyCode === 39 || e.keyCode === 37) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+        if (e.keyCode === 39) {
+          tabFocus++;
+
+          if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+          }
+        } else if (e.keyCode === 37) {
+          tabFocus--;
+
+          if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+          }
+        }
+
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
+      }
+    });
+  }
+})();
