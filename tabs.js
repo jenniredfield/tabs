f065d878956d63c6c22b540b8010d165493e7dae
Jen Bird <jenniredfield@hotmail.com>
2020-07-09 18:24:59 +0100

    All tabs and tabpanels accessible in the script file

--- tabs.js
+++ tabs.js
@@ -1,1 +1,10 @@
 console.log('log from tabs.js');
+
+document.onload  = (() => {
+
+    const tabs = document.querySelectorAll('[role="tab"]');
+    console.log("document.onload -> tabs", tabs)
+    const contentEls = document.querySelectorAll('[role="tabpanel"]');
+    console.log("document.onload -> contentEls", contentEls)
+
+})()
