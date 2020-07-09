d4fec38c8eba50f5ae06edfb34170f114525174b
Jen Bird <jenniredfield@hotmail.com>
2020-07-09 18:33:27 +0100

    Functionality to change tabs working

--- tabs.js
+++ tabs.js
@@ -5,6 +5,29 @@ document.onload  = (() => {
     const tabs = document.querySelectorAll('[role="tab"]');
     console.log("document.onload -> tabs", tabs)
     const contentEls = document.querySelectorAll('[role="tabpanel"]');
-    console.log("document.onload -> contentEls", contentEls)
+    console.log("document.onload -> contentEls", contentEls);
+
+
+    function updateTab(e) {
+        // update tab selected
+        const targetId = e.target.id;
+
+        tabs.forEach(tab => {
+            const tabId = tab.getAttribute('id');
+            if(tabId === targetId) {
+                tab.setAttribute('aria-selected', true);
+                return;
+            }
+
+            tab.setAttribute('aria-selected', false);
+        });
+
+        // update content selected
+
+    }
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', updateTab);
+    })
 
 })()
